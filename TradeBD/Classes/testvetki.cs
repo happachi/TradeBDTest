055b64de3148b24370ae4bdb7ed4637f24e79194
happachi <syxoq0@mail.ru>
2026-04-14 09:21:39 +0300

    класс testverki

---
+++ TradeBD/Classes/testvetki.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeBD.Classes
+{
+    internal class testvetki
+    {
+        /// dfp[w[fpewfpwpfwepfwefwe
+    }
+}
